a6438c86daaddbda0869f6267d9f9d945e9870f8
KingMyxos <74276565+KingMyxos@users.noreply.github.com>
2025-09-21 06:58:46 -0700

    Add files via upload

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a href='https://t.me/Yrrzn' className='home__social-icon'>
+        <i className='uil uil-telegram'></i>
+      </a>
+
+      <a href='https://www.instagram.com/lemiel._' className='home__social-icon'>
+        <i className='uil uil-instagram'></i>
+      </a>
+
+      <a href='https://github.com/KingMyxos' className='home__social-icon'>
+        <i className='uil uil-github-alt'></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
