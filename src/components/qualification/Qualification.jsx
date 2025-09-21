3f84d2187e08af473c0af7051cb2543cbb022cfe
KingMyxos <74276565+KingMyxos@users.noreply.github.com>
2025-09-21 06:59:11 -0700

    Add files via upload

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import "./qualification.css";
+
+const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My personel journey</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Education
+          </div>
+
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Teknik Komputer dan Jaringan</h3>
+                <span className="qualification__subtitle">
+                  SMK Negeri 56 Jakarta
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - 2025
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Student</h3>
+                <span className="qualification__subtitle">
+                  SMP Islam Bahagia
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2019 - 2022
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Student</h3>
+                <span className="qualification__subtitle">
+                  SD Negeri 02 Kalianyar
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2012 - 2018
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+            </div>
+          </div>
+
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Network Support (PKL)</h3>
+                <span className="qualification__subtitle">
+                  Kementerian Agama
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> July - December 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+            </div>
+
+            <div className="qualification__data">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
